75074376db82fe85b570594fbfffb723045a9e57
alx <acriptis@gmail.com>
2019-05-21 17:54:27 +0300

    add usage script

--- experiment.py
+++ experiment.py
@@ -1,15 +1,28 @@
-# site:wikipedia.org Когда родился Пушкин?
+import os
 from google_search import GoogleCustomSearch
 
+# define environmental variables:
+try:
+    SEARCH_ENGINE_ID = os.environ['SEARCH_ENGINE_ID']
+except Exception as e:
+    print("Missed Environmental variable SEARCH_ENGINE_ID. "
+          "Retrieve Search Engine ID at https://cse.google.com/")
+    raise e
 
-# SEARCH_ENGINE_ID = os.environ['SEARCH_ENGINE_ID']
-SEARCH_ENGINE_ID = "005711384174884746282:l1hoqyznj4a"
-# API_KEY = os.environ['GOOGLE_CLOUD_API_KEY']
-API_KEY = "AIzaSyBknezKiE9xznOQ1ZsWry5K-s1IdfK_Rwc"
+try:
+    API_KEY = os.environ['GOOGLE_CLOUD_API_KEY']
+except Exception as e:
+    print("Missed Environmental variable GOOGLE_CLOUD_API_KEY. "
+          "Retrieve GOOGLE_CLOUD_API_KEY at "
+          "https://developers.google.com/custom-search/v1/introduction"
+
+    raise e
+
+
 
 api = GoogleCustomSearch(SEARCH_ENGINE_ID, API_KEY)
-import ipdb; ipdb.set_trace()
 
+# sample output
 for result in api.search("Когда родился Пушкин?"):
     print("*"*80)
     print("Title")
